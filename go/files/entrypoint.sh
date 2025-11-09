61d7578769379805d296e3c923161f2ccd8b0769
joshw34 <joshwhitley@msn.com>
2025-11-09 17:05:07 +0100

    changed fish->zsh for all. mounted  host ssh_auth_sock

--- go/files/entrypoint.sh
+++ go/files/entrypoint.sh
@@ -1,50 +1,53 @@
 #!/bin/bash
 set -e  # Exit on error
 
-FISH_CONFIG_DIR="$HOME/.config/fish"
-FISH_CONFIG_FILE="$FISH_CONFIG_DIR/config.fish"
+ZSHRC="$HOME/.zshrc"
+OMZ_CUSTOM="$HOME/.oh-my-zsh/custom"
+P10K_DIR="$OMZ_CUSTOM/themes/powerlevel10k"
+AUTOSUGGEST_DIR="$OMZ_CUSTOM/plugins/zsh-autosuggestions"
+HIGHLIGHTING_DIR="$OMZ_CUSTOM/plugins/zsh-syntax-highlighting"
 GO_BIN_SYS="/usr/local/go/bin"
-GO_BIN_LOCAL="$HOME/go/bin/"
-
-if [ ! -f "$FISH_CONFIG_FILE" ]; then
-  mkdir -p "$FISH_CONFIG_DIR"
-  touch "$FISH_CONFIG_FILE"
-  echo "set fish_greeting" > "$FISH_CONFIG_FILE"
-  echo "fish_config prompt choose astronaut" >> "$FISH_CONFIG_FILE"
-  echo 'fish_config theme choose "Catppuccin Mocha"' >> "$FISH_CONFIG_FILE"
-  git clone https://github.com/catppuccin/fish.git "$HOME"/catppuccin_theme
-  mkdir -p "$FISH_CONFIG_DIR"/themes
-  mv "$HOME"/catppuccin_theme/themes/"Catppuccin Mocha.theme" "$FISH_CONFIG_DIR"/themes
-  rm -rf "$HOME"/catppuccin_theme
-  echo "set PATH $PATH:$GO_BIN_SYS:$GO_BIN_LOCAL" >> "$FISH_CONFIG_FILE"
-fi
-
-#Temp export for script to install gopls
-export PATH=$PATH:$GO_BIN_SYS:$GO_BIN_LOCAL
-#Install go_pls
-go install golang.org/x/tools/gopls@latest
+GO_BIN_LOCAL="$HOME/go/bin"
 
 # Configure git
 echo "Configuring git..."
 git config --global user.email "joshw34@joshw34.com"
 git config --global user.name "joshw34"
 
-# Configure SSH agent
-if ! grep -q 'SSH_AUTH_SOCK' "$FISH_CONFIG_FILE"; then
-  echo "Configuring SSH agent..."
-  cat >> "$FISH_CONFIG_FILE" << 'EOF'
-# SSH Agent
-if not set -q SSH_AUTH_SOCK
-    eval (ssh-agent -c) > /dev/null
-    test -f ~/.ssh/github; and ssh-add ~/.ssh/github 2>/dev/null
-end
-EOF
+# Create .zshrc if not already present
+if [ ! -f "$ZSHRC" ]; then
+  touch "$ZSHRC"
+fi
+
+# Install oh-my-zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+# Install powerlevel10k and set .zshrc theme
+if [ ! -d "$P10K_DIR" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$P10K_DIR"
+  sed -i '/^ZSH_THEME/c\ZSH_THEME="powerlevel10k/powerlevel10k"' "$ZSHRC"
+fi
+
+# Install zsh-autosuggestions
+if [ ! -d "$AUTOSUGGEST_DIR" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$AUTOSUGGEST_DIR"
+fi
+
+# Install zsh-syntax-highlighting
+if [ ! -d "$HIGHLIGHTING_DIR" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HIGHLIGHTING_DIR"
+fi
+
+# Add zsh-autosuggestions and zsh-syntax-highlighting and add to .zshrc plugins
+if ! grep -q 'plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' "$ZSHRC"; then
+  sed -i '/^plugins=/c\plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' "$ZSHRC"
 fi
 
 # Configure eza aliases
-if ! grep -q 'eza aliases' "$FISH_CONFIG_FILE"; then
-  echo "Configuring eza aliases..."
-  cat >> "$FISH_CONFIG_FILE" << 'EOF'
+if ! grep -q 'eza aliases' "$ZSHRC"; then
+  cat >> "$ZSHRC" << 'EOF'
 
 # eza aliases
 alias ls="eza -1 --icons=always"
@@ -60,4 +63,14 @@ alias lsat="eza -a -1T --icons=always"
 EOF
 fi
 
-exec fish
+#Temp export for script to install gopls
+export PATH="$PATH:$GO_BIN_SYS:$GO_BIN_LOCAL"
+#Install go_pls
+go install golang.org/x/tools/gopls@latest
+
+# Add go paths to $PATH (needs to be added after zsh configuration so it isnt overwritten)
+if ! grep -q 'export PATH=$PATH' "$ZSHRC"; then
+  echo "export PATH=\$PATH:$GO_BIN_SYS:$GO_BIN_LOCAL" >> "$ZSHRC"
+fi
+
+exec zsh
