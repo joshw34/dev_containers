b9818c42e4197f297a241d3b05055549028b48d0
joshw34 <joshwhitley@msn.com>
2026-01-18 12:27:27 +0100

    added sshd_config

--- dockerfiles/cpp/files/start_ssh.sh
+++ dockerfiles/cpp/files/start_ssh.sh
@@ -10,10 +10,6 @@ if [ -d /root/.ssh ]; then
     cat /root/.ssh/*.pub > /root/.ssh-writable/authorized_keys 2>/dev/null
     chmod 700 /root/.ssh-writable
     chmod 600 /root/.ssh-writable/authorized_keys
-    # shellcheck disable=SC1073
-    echo 'AuthorizedKeysFile /root/.ssh-writable/authorized_keys' >> /etc/ssh/sshd_config
-    echo 'PermitRootLogin prohibit-password' >> /etc/ssh/sshd_config
-    echo 'PubkeyAuthentication yes' >> /etc/ssh/sshd_config
 fi
 
 mkdir -p /run/sshd
