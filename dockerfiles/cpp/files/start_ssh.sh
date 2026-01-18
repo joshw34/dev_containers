c3c6e0955a47c8fef1e30dbd4cd00dca8c89f648
joshw34 <joshwhitley@msn.com>
2026-01-18 11:26:30 +0100

    added ssh

---
+++ dockerfiles/cpp/files/start_ssh.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
+    ssh-keygen -A
+fi
+
+if [ -d /root/.ssh ]; then
+    mkdir -p /root/.ssh-writable
+    cat /root/.ssh/*.pub > /root/.ssh-writable/authorized_keys 2>/dev/null
+    chmod 700 /root/.ssh-writable
+    chmod 600 /root/.ssh-writable/authorized_keys
+    # shellcheck disable=SC1073
+    echo 'AuthorizedKeysFile /root/.ssh-writable/authorized_keys' >> /etc/ssh/sshd_config
+    echo 'PermitRootLogin prohibit-password' >> /etc/ssh/sshd_config
+    echo 'PubkeyAuthentication yes' >> /etc/ssh/sshd_config
+fi
+
+mkdir -p /run/sshd
+
+exec /usr/sbin/sshd -D
+
