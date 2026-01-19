7509bb8d158df7079aa84f6246811b6a56325b36
joshw34 <159394665+joshw34@users.noreply.github.com>
2026-01-19 20:00:48 +0100

    Fix SSH setup script for authorized_keys file

--- dockerfiles/cpp/files/start_ssh.sh
+++ dockerfiles/cpp/files/start_ssh.sh
@@ -5,14 +5,11 @@ if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
     ssh-keygen -A
 fi
 
-if [ -d /root/.ssh ]; then
-    mkdir -p /root/.ssh-writable
-    cat /root/.ssh/*.pub > /root/.ssh-writable/authorized_keys 2>/dev/null
-    chmod 700 /root/.ssh-writable
-    chmod 600 /root/.ssh-writable/authorized_keys
+if [ ! -d /root/.ssh ]; then
+    mkdir -p /root/.ssh
+    touch /root/.ssh/authorized_keys
 fi
 
 mkdir -p /run/sshd
 
 exec /usr/sbin/sshd -D
-
